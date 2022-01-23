0d91ae059bc0f0d5b07e21773a41c9a534e8a933
tianrking <tian.r.king@gmail.com>
2022-01-23 20:58:34 +0800

    Modular Optimized Readable

--- test/a_welcome.js
+++ test/a_welcome.js
@@ -4,12 +4,8 @@ import { textIntentDetect } from '../src/intent-detect.js' //测试
 import { bot } from '../src/bot.js'
 import { url } from 'inspector'
 import fs from 'fs'
-// import { onMessage } from '../src/index.js'
-
 
 try {
-    // let r = await hello();
-    // assert.strictEqual(r, 15);
     console.log("      😊  w0x7ce")
     console.log(' Juzibot v1.0 测试 🔥');
     console.log(' 纯文本 意图测试src/intent-detect.js')
--- test/myfile_test.js
+++ test/myfile_test.js
@@ -13,7 +13,7 @@ describe('myfile_test.js 👋',() => {
 
     const args_8 = {
         text: () => {
-            return '我的文件 txt.jpg'
+            return '我的文件'
         }
     }
     let payload_8 = {
@@ -21,16 +21,12 @@ describe('myfile_test.js 👋',() => {
         isRoom: false,
         text: args_8.text()
     }
-    var _expected_value_8 = {
-        roomTopic: null,
-        isRoom: false,
-        text: '我的文件 txt.jpg',
-        intent: 'todo'
-      }
+    var _expected_value_8 = { roomTopic: null, isRoom: false, text: '我的文件', intent: 'list-file' }
+    
     it('💰我的文件', async () => {
         
         const _res_8 = await textIntentDetect(args_8,payload_8); 
-        //console.log(_res_8);
+        console.log(_res_8);
         assert.deepEqual(_res_8,_expected_value_8);
     });
 });
